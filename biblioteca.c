731bb23e6ab404bfd6481d0c8fbcb43fb9567f87
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-04 13:04:08 -0300

    adicionar e remover

--- biblioteca.c
+++ biblioteca.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 /*
 Recebe o nome de um livro e retorna seu id
@@ -17,7 +18,7 @@ int id_nome(char nome[100]){
 /*
 Funcao para adicionar ou remover um livro pelo seu ID
 */
-int adicionar_remover(int livros[100][3], char *nome_livros[100], int contador){
+int adicionar_remover(int livros[100][4], char nome_livros[][20], int contador){
     int aux = 0, flag = 1;
 
     while(flag != 2){
@@ -27,7 +28,7 @@ int adicionar_remover(int livros[100][3], char *nome_livros[100], int contador){
         //adiciona um livro
         if(aux == 1){
             char nome[100];
-            int id;
+            int id, flag = 1;
             printf("Insira o nome do livro\n");
             scanf("%s", nome);
             id = id_nome(nome);
@@ -36,15 +37,22 @@ int adicionar_remover(int livros[100][3], char *nome_livros[100], int contador){
             for(int i = 0; i<contador; i++){
                 if(livros[i][0] == id){
                     printf("Esse livro ja esta cadastrado\n");
+                    flag = 0;
                 }
             }
             //verifica se ja atingiu o limite
             if(contador == 100){
                 printf("Nao existe mais espaço na biblioteca\n");
             }
-            else{
+            //se o livro nao estiver cadastrado, cadastra ele
+            else if(flag != 0){
                 livros[contador][0] = id;
-                nome_livros[contador] = nome;
+                livros[contador][1] = 0;
+                livros[contador][2] = 0;
+                livros[contador][3] = 0;
+                for(int n=0; n<=strlen(nome); n++){
+                    nome_livros[contador][n] = nome[n];
+                }
                 contador++;
                 printf("Adicionado com sucesso!\n");
             }
@@ -70,7 +78,6 @@ int adicionar_remover(int livros[100][3], char *nome_livros[100], int contador){
                         livros[aux][1] = livros[aux+1][1];
                         livros[aux][2] = livros[aux+1][2];
                         livros[aux][3] = livros[aux+1][3];
-                        nome_livros[aux] = nome_livros[aux+1];
                     }
                     contador--;
                     printf("Removido com sucesso!\n");
@@ -99,27 +106,31 @@ int main(){
     //livros[pos][0] = ID
     //livros[pos][1] = qtd de vezes alugado 
     //livros[pos][2] = qtd em estoque
-    //livros[pos][3] = nota 
-    int livros[100][3], contador_livros = 0;
+    //livros[pos][3] = nota de 0-100
+    int livros[100][4], contador_livros = 0;
 
-    //nome_livros[pos] onde cada pos armazena uma string com o nome do livro
-    char *nome_livros[100];
+    //nome_livros[pos] onde cada pos armazena uma string de tamanho 20
+    char nome_livros[100][20];
     
-    printf("Ola boa, seja bem-vindo a Biblioteca UFABC! Voce gostaria de acessar nosso sistema como?\nDigite 1 - Cliente\nDigite 2 - Funcionario\n");
-    while(flag == 0){
+    
+    while(flag != 2){
+
+        printf("Ola seja bem-vindo a Biblioteca UFABC! Voce gostaria de acessar nosso sistema como?\nDigite 1 - Cliente\nDigite 2 - Funcionario\n");
         scanf("%d", &aux);
+
         if(aux == 1){
             printf("Ainda estamos preparando o sistema para nossos clientes\n");
-            flag = 1;
         }
         else if(aux == 2){
             printf("Ainda estamos preparando o sistema para nossos funcionarios\n");
             livros, contador_livros = adicionar_remover(livros, nome_livros, contador_livros);
-            flag = 1;
         }
         else{
             printf("Essa entrada nao eh valida\n");
         }
+
+        printf("Voce deseja fazer mais alguma coisa?\n1->sim\n2->nao\n");
+        scanf("%d", &flag);
     }
 
     return 0;
