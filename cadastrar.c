8fe6ed9f3129a6229522c0162b745015d2e07dc5
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-10 13:32:42 -0300

    atualizado

--- cadastrar.c
+++ cadastrar.c
@@ -1,24 +1,10 @@
-#include<stdio.h>
-#include<string.h>
-
-/*
-Recebe o nome de um livro e retorna seu id
-o id é calculado pela soma do valor int das letras que compoem o nome do livro 
-*/
-int id_nome(char nome[100]){
-    int id;
-
-    for(int i=0; nome[i]!='\0'; i++){
-        id += nome[i];
-    }
-    
-    return id;
-}
+#include "biblioteca.h"
 
 /*
 Funcao para adicionar ou remover um livro pelo seu ID
 */
-int adicionar_remover(int livros[100][4], char nome_livros[][20], int contador){
+int adicionar_remover(int livros[100][5], char nome_livros[][20], int contador)
+{
     int aux = 0, flag = 1;
 
     while(flag != 2){
@@ -78,6 +64,7 @@ int adicionar_remover(int livros[100][4], char nome_livros[][20], int contador){
                         livros[aux][1] = livros[aux+1][1];
                         livros[aux][2] = livros[aux+1][2];
                         livros[aux][3] = livros[aux+1][3];
+                        livros[aux][4] = livros[aux+1][4];
                     }
                     contador--;
                     printf("Removido com sucesso!\n");
@@ -96,42 +83,4 @@ int adicionar_remover(int livros[100][4], char nome_livros[][20], int contador){
     }
 
     return livros, contador;
-}
-
-int main(){
-
-    int aux = -1, flag = 0;
-
-    //vetor com id de todos os livros e o contador com quantos livros tem:
-    //livros[pos][0] = ID
-    //livros[pos][1] = qtd de vezes alugado 
-    //livros[pos][2] = qtd em estoque
-    //livros[pos][3] = nota de 0-100
-    int livros[100][4], contador_livros = 0;
-
-    //nome_livros[pos] onde cada pos armazena uma string de tamanho 20
-    char nome_livros[100][20];
-    
-    
-    while(flag != 2){
-
-        printf("Ola seja bem-vindo a Biblioteca UFABC! Voce gostaria de acessar nosso sistema como?\nDigite 1 - Cliente\nDigite 2 - Funcionario\n");
-        scanf("%d", &aux);
-
-        if(aux == 1){
-            printf("Ainda estamos preparando o sistema para nossos clientes\n");
-        }
-        else if(aux == 2){
-            printf("Ainda estamos preparando o sistema para nossos funcionarios\n");
-            livros, contador_livros = adicionar_remover(livros, nome_livros, contador_livros);
-        }
-        else{
-            printf("Essa entrada nao eh valida\n");
-        }
-
-        printf("Voce deseja fazer mais alguma coisa?\n1->sim\n2->nao\n");
-        scanf("%d", &flag);
-    }
-
-    return 0;
 }
---
+++ id.c
@@ -0,0 +1,15 @@
+#include "biblioteca.h"
+
+/*
+Recebe o nome de um livro e retorna seu id
+o id é calculado pela soma do valor int das letras que compoem o nome do livro 
+*/
+int id_nome(char nome[100]){
+    int id;
+
+    for(int i=0; nome[i]!='\0'; i++){
+        id += nome[i];
+    }
+    
+    return id;
+}
