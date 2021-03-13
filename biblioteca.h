989960161553f56dcb91457ce8afb4ecdc82665f
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-13 14:56:31 -0300

    adicionado catalogo e estoque

--- biblioteca.h
+++ biblioteca.h
@@ -8,5 +8,7 @@ int adicionar_remover(int livros[100][5], char nome_livros[][20], int contador);
 int id_nome(char nome[100]);
 void avaliar(int livros[100][5]);
 void detalhes(int livros[100][5]);
+void catalogo(int livros[100][5], char nome_livros[100][20]);
+void estoque(int livros[100][5], char nome_livros[100][20], int qtde);
 
 #endif // BIBLIOTECA_H
