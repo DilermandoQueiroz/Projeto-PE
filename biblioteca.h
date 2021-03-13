98543b224cffab35260181419cc6a26694c6ad65
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-13 17:32:03 -0300

    atualizado

--- biblioteca.h
+++ biblioteca.h
@@ -5,7 +5,7 @@
 #include<string.h>
 #include<stdlib.h>
 
-int adicionar_remover(int livros[100][5], char nome_livros[][20], int contador);
+int adicionar_remover(int livros[100][6], char nome_livros[][20], int contador);
 int id_nome(char nome[100]);
 void avaliar(int livros[100][5]);
 void detalhes(int livros[100][5]);
