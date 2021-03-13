09b0cc5c6882f247aa318292e490fe476918e03d
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-13 17:00:54 -0300

    i=0

--- id.c
+++ id.c
@@ -5,7 +5,7 @@ Recebe o nome de um livro e retorna seu id
 o id é calculado pela soma do valor int das letras que compoem o nome do livro 
 */
 int id_nome(char nome[100]){
-    int id;
+    int id = 0;
 
     for(int i=0; nome[i]!='\0'; i++){
         id += nome[i];
