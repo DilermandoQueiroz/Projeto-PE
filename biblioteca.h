eaa4500a7eaf44cf5d90bb4928c43725a7c0e0dc
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-15 17:56:17 -0300

    adc avaliar, detalhes

--- biblioteca.h
+++ biblioteca.h
@@ -7,8 +7,8 @@
 
 int adicionar_remover(int livros[100][6], char nome_livros[][20], int contador);
 int id_nome(char nome[100]);
-void avaliar(int livros[100][5]);
-void detalhes(int livros[100][5]);
+int avaliar(int livros[100][6], int contador);
+void detalhes(int livros[100][6], int contador);
 void catalogo(int livros[100][5], char nome_livros[100][20]);
 void estoque(int livros[100][5], char nome_livros[100][20], int qtde);
 int AlugarComprar(int livros[100][6], int contador);
