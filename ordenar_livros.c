2b24215998df465aac6568bbadf6bd4f92c2aaf0
Marcus Vinicius Holanda de Lima <marcus.vinicius.99@hotmail.com>
2021-03-15 15:42:37 -0300

    conserto de erro de ordenação de palavras com letras maiusculas e minusculas

--- ordenar_livros.c
+++ ordenar_livros.c
@@ -1,6 +1,6 @@
 #include "biblioteca.h"
 
-void troca(char p1[100][20], int palavra){
+void troca(char p1[100][20], int palavra){ //troca a palavra X com a palavra X+1
     char temp[20];
     strcpy(temp, p1[palavra]);
     strcpy(p1[palavra], p1[palavra+1]);
@@ -8,7 +8,16 @@ void troca(char p1[100][20], int palavra){
     
 }
 
-void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde){
+char converte_maiuscula(char letra){ // converte uma letra maiuscula em minuscula
+    if (letra>=65 && letra<=90)
+    {
+        letra= letra+32;
+    }
+    
+    return letra;
+}
+
+ void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde){
     char ordenado[qtde][20];  //vetor temporário para receber livros em ordem, será apagado no fim da função
     int count = 0;
 
@@ -22,26 +31,22 @@ void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde){
 
     while(count!=qtde-1) //o count é quantidade de vezes que um dupla de palavras está na ordem certa, em um vetor de comprimento "qtde" a quatidade de duplas é "qtde-1"
     {
+        
         count=0;
 
-        for (int x = 0; x < qtde-1; x++) //como é verificado n e n+1, é necessário -1 para não comparar com um valor não determinado
+        for (int x = 0; x < qtde-1; x++) // percorrer o vetor
         {
             int comp;
             if(strlen(ordenado[x])<strlen(ordenado[x+1])) { // achar o menor comprimento
                 comp=strlen(ordenado[x]);
-            }else
-            {  
-                comp=strlen(ordenado[x+1]); 
-            }
-
-            int n=0;
-
-            while (ordenado[x][n]==ordenado[x+1][n] && n<comp) 
+            }else{  comp=strlen(ordenado[x+1]); }
+                int n=0;
+            while (converte_maiuscula(ordenado[x][n])==converte_maiuscula(ordenado[x+1][n]) && n<comp) // como é verificado n e n+1, é necessário -1 para não comparar com um valor não determinado
                 {
                     n++;
                 } 
 
-            if (ordenado[x][n]>ordenado[x+1][n]) // troca 
+            if (converte_maiuscula(ordenado[x][n]) >converte_maiuscula(ordenado[x+1][n]) ) // troca 
             {
                 
                 troca(ordenado,x);
