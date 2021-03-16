ece88160bba39a6627e0fd096d300fe59e56df16
Leticia Namie <leticia.namie@hotmail.com>
2021-03-16 19:16:51 -0300

    Incluindo biblioteca_h e main atualizados

--- biblioteca.h
+++ biblioteca.h
@@ -5,13 +5,13 @@
 #include<string.h>
 #include<stdlib.h>
 
-int adicionar_remover(int livros[100][6], char nome_livros[][20], int contador);
+int adicionar_remover(int livros[100][6], char nome_livros[100][20], int contador);
 int id_nome(char nome[100]);
 int avaliar(int livros[100][6], int contador);
 void detalhes(int livros[100][6], int contador);
-void catalogo(int livros[100][5], char nome_livros[100][20]);
-void estoque(int livros[100][5], char nome_livros[100][20], int qtde);
-int AlugarComprar(int livros[100][6], int contador);
+void catalogo(int livros[100][6], char nome_livros[100][20], int qtde);
+void estoque(int livros[100][6], char nome_livros[100][20], int qtde);
+int AlugarComprar(int livros[100][6], char nome_livros[100][20], int contador);
 int Devolver(int livros[100][6], int contador);
 void troca(char p1[100][20], int palavra);
 void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde);
