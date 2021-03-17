959675185b0c7d446cb31cc430380565d0d9ae62
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-16 21:20:44 -0300

    Francisca

--- alugarcomprar_devolver.c
+++ alugarcomprar_devolver.c
@@ -1,31 +1,42 @@
 #include "biblioteca.h"
 
-//Fun��o para aluguel/compra de livros
-int AlugarComprar(int livros[100][6], int contador){
+//Funcao para aluguel/compra de livros
+int AlugarComprar(int livros[100][6], char nome_livros[100][20], int contador){
 
-    int idlivro, aux=0, op=0;
+    int idlivro, aux=0, op=0, escolha = 0;
     char nome[100];
 
-    //Mostra os catalogo
-    //Catalogo();
+    printf("\n Deseja ver o cat�logo antes? \n Digite 1 - Sim \n Digite 2 - Nao\n");
+    scanf("%i", &escolha);
 
+    do{
+        if(escolha == 1){
+            catalogo(livros, nome_livros, contador);
+
+            escolha = 2;
+        }
+        else if(escolha != 2){
+            printf("\n Essa entrada nao eh valida\n");
+        }
+
+    }while(escolha!= 2);
 
     while(op!=3){
 
-        printf("\nOque deseja fazer:\n1-Alugar um livro\n2-Comprar um livro\n3-Sair\n");
+        printf("\n O que deseja fazer: \n 1-Alugar um livro \n 2-Comprar um livro \n 3-Sair\n");
         scanf("%i", &op);
 
 
         if(op==1){
-            printf("\nPor favor digite o nome do livro que deseja alugar: \n");
+            printf("\n Por favor digite o nome do livro que deseja alugar: \n");
             fflush(stdin);
             gets(nome);
 
             idlivro = id_nome(nome);
 
             for(int i = 0; i<contador; i++){
-                if(livros[i][0]==idlivro){
-                    printf("\nO livro %s alugado com sucesso, tenha uma otima leitura!\n", nome);
+                if(livros[i][0] == idlivro){
+                    printf("\n O livro %s alugado com sucesso, tenha uma otima leitura! \n", nome);
                     //Aumenta o contador de alugueis
                     livros[i][1]++;
                     //Diminue a quatidade em estoque
@@ -33,11 +44,12 @@ int AlugarComprar(int livros[100][6], int contador){
                     aux=1;
                 }
             }
+            printf("\n Voce saiu da sessao de compras/alugueis! \n");
         }
 
-        if(op==2){
+        else if(op==2){
             aux=0;
-            printf("\nPor favor digite o nome do livro que deseja comprar: \n");
+            printf("\n Por favor digite o nome do livro que deseja comprar: \n");
             fflush(stdin);
             gets(nome);
 
@@ -45,18 +57,22 @@ int AlugarComprar(int livros[100][6], int contador){
 
             for(int i = 0; i<contador; i++){
                 if(livros[i][0]==idlivro){
-                    printf("\nO livro %s comprado com sucesso, agradecemos a preferencia e boa leitura!\n", nome);
+                    printf("\n O livro %s comprado com sucesso, agradecemos a preferencia e boa leitura!\n", nome);
                     //Aumenta o contador de compras
                     livros[i][5]++;
-                    //Diminue a quatidade em estoque
+                    //Diminui a quatidade em estoque
                     livros[i][2]--;
                     aux=1;
                 }
             }
         }
 
+        else if(op!= 3){
+            printf("\n Essa entrada nao eh valida\n");
+        }
+
         if (aux==0 && op!=3){
-            printf("\nDesculpe mas o livro %s nao foi encontrado.\n", nome);
+            printf("\n Desculpe mas o livro %s nao foi encontrado.\n", nome);
             idlivro=0;
         }
     }
@@ -64,7 +80,7 @@ int AlugarComprar(int livros[100][6], int contador){
     return livros, contador;
 }
 
-//Fun��o pra devolver livros alugados
+///Funcao pra devolver livros alugados
 int Devolver(int livros[100][6], int contador){
     int idlivro, aux=0, op=0;
     char nome[100];
@@ -72,7 +88,7 @@ int Devolver(int livros[100][6], int contador){
 
     while(aux==0){
 
-        printf("\nPor favor digite o nome do livro que deseja devolver \n");
+        printf("\n Por favor digite o nome do livro que deseja devolver \n");
         fflush(stdin);
         gets(nome);
 
@@ -80,26 +96,26 @@ int Devolver(int livros[100][6], int contador){
 
         for(int i = 0; i<contador; i++){
             if(livros[i][0]==idlivro){
-                printf("\nO livro %s foi devolvido com sucesso!\n", nome);
+                printf("\n O livro %s foi devolvido com sucesso!\n", nome);
+
                 //Aumenta a quatidade em estoque
                 livros[i][2]++;
                 aux=1;
-                printf("\nDeseja avaliar o livro?\n1 - Sim\n2 - Nao\n");
+
+                printf("\n Deseja avaliar o livro?\n1 - Sim\n2 - Nao\n");
                 scanf("%i", &op);
+
                 if(op == 1){
-                    //Chamar fun�ao de avalia��o
-                    //avaliar(livros);
+                    avaliar(livros, contador);
                 }
             }
         }
         if(aux==0){
-            printf("\nDesculpe mas o livro %s nao foi encontrado.\n", nome);
+            printf("\n Desculpe mas o livro %s nao foi encontrado.\n", nome);
             idlivro=0;
         }
     }
 
     return livros, contador;
-
-
 
 }
