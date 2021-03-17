27076f9e0da8699cbb0e1a93d30bf5813590b2ac
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-03-16 21:18:42 -0300

    Marcus

--- ordenar_livros.c
+++ ordenar_livros.c
@@ -5,7 +5,7 @@ void troca(char p1[100][20], int palavra){ //troca a palavra X com a palavra X+1
     strcpy(temp, p1[palavra]);
     strcpy(p1[palavra], p1[palavra+1]);
     strcpy(p1[palavra+1], temp);
-    
+
 }
 
 char converte_maiuscula(char letra){ // converte uma letra maiuscula em minuscula
@@ -13,12 +13,12 @@ char converte_maiuscula(char letra){ // converte uma letra maiuscula em minuscul
     {
         letra= letra+32;
     }
-    
+
     return letra;
 }
 
- void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde){
-    char ordenado[qtde][20];  //vetor temporário para receber livros em ordem, será apagado no fim da função
+  void ordenar_livros(int livros[100][6], char nome_livros[100][20],int qtde){
+    char ordenado[qtde][20];  //vetor tempor�rio para receber livros em ordem, ser� apagado no fim da fun��o
     int count = 0;
 
     for (int i = 0 ; i < qtde; i++)
@@ -29,41 +29,38 @@ char converte_maiuscula(char letra){ // converte uma letra maiuscula em minuscul
     }
 
 
-    while(count!=qtde-1) //o count é quantidade de vezes que um dupla de palavras está na ordem certa, em um vetor de comprimento "qtde" a quatidade de duplas é "qtde-1"
+    while(count!=qtde-1) //o count � quantidade de vezes que um dupla de palavras est� na ordem certa, em um vetor de comprimento "qtde" a quatidade de duplas � "qtde-1"
     {
         count=0;
 
-        for (int x = 0; x < qtde-1; x++) //como é verificado n e n+1, é necessário -1 para não comparar com um valor não determinado
+        for (int x = 0; x < qtde-1; x++) //como � verificado n e n+1, � necess�rio -1 para n�o comparar com um valor n�o determinado
         {
             int comp;
             if(strlen(ordenado[x])<strlen(ordenado[x+1])) { // achar o menor comprimento
                 comp=strlen(ordenado[x]);
             }else{  comp=strlen(ordenado[x+1]); }
                 int n=0;
-            while (converte_maiuscula(ordenado[x][n])==converte_maiuscula(ordenado[x+1][n]) && n<comp) // como é verificado n e n+1, é necessário -1 para não comparar com um valor não determinado
+            while (converte_maiuscula(ordenado[x][n])==converte_maiuscula(ordenado[x+1][n]) && n<comp) // como � verificado n e n+1, � necess�rio -1 para n�o comparar com um valor n�o determinado
                 {
                     n++;
-                } 
+                }
 
-            if (converte_maiuscula(ordenado[x][n]) >converte_maiuscula(ordenado[x+1][n]) ) // troca 
+            if (converte_maiuscula(ordenado[x][n]) >converte_maiuscula(ordenado[x+1][n]) ) // troca
             {
-                
                 troca(ordenado,x);
 
             }else
             {
                 count++;
             }
-              
+
         }
-        
 
     }
-    
+
     for (int i = 0; i < qtde; i++)
     {
         printf("%s\n", ordenado[i]);
     }
-
 
 }
