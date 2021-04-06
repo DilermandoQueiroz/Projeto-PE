7c38ad4089e152cd129cdc93bee6d59ee5bf0d55
Dilermando Queiroz Neto <dilermando.queiroz@aluno.ufabc.edu.br>
2021-04-06 20:01:24 -0300

    adicionado struct para livros e cliente

--- biblioteca.h
+++ biblioteca.h
@@ -5,6 +5,22 @@
 #include<string.h>
 #include<stdlib.h>
 
+struct livros{
+    char nome[50];
+    int id;
+    int qtd_alugado;
+    int qtd_estoque;
+    int avaliacao, qtd_avaliacoes;
+};
+
+struct cliente{
+    char nome[50];
+    int id;
+    char livros_alugado[20][50];
+    char usuario[10];
+    char senha[10];
+};
+
 int adicionar_remover(int livros[100][6], char nome_livros[100][20], int contador);
 int id_nome(char nome[100]);
 int avaliar(int livros[100][6], int contador);
